110528db8d69e23c11b71834c7d9104a1ed40208
muturich <calvinrich20@gmail.com>
2022-11-23 14:13:07 -0600

    add some script

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,6 @@ const newName = document.getElementById ("cityInput");
 const cityName = document.getElementById ("cityName");
 cityName.innerHTML ="--"+newName.value+"--"
 }
-
 fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=ed61eb31d1ec0933f7ad88885d73f8bc")
 .then (response => response.json ())
 .then (data =>{
@@ -11,6 +10,32 @@ fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appi
         document.getElementById("day" + (i+1)+ "Min").innerHTML = "Min:" + Number(data.list[i].main.temp_min -278.42).toFixed(1)+"°";
     }
     for (i=0;i<5;i++){
-        document.getElementById("day" + (i+1)+ "Man").innerHTML = "Man:" + Number(data.list[i].main.temp_min -279.26).toFixed(1)+"°";
+        document.getElementById("day" + (i+1)+ "Max").innerHTML = "Max:" + Number(data.list[i].main.temp_max -279.26).toFixed(1)+"°";
+    }
+   
+    for (i=0;i<5;i++){
+        document.getElementById("img" +(i+1)).src=" http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon+".png";
     }
 })
+.catch(err => alert("Something Went Wrong"))
+
+
+function DefaultScreen(){
+    document.getElementById("cityInput").defaultValue ="Kigali";
+    Getinfo();
+}
+const d=new Date();
+const weekday=["Sundays","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+function CheckDay(day){
+    if (day +d.getDay()>6){
+        return day +d.getDay() -7;
+    }
+    else{
+        return day +d.getDay();
+    }
+}
+
+for (i=0;i<5;i++){
+    document.getElementById("day"+(i+1)).innerHTML=weekday[CheckDay(i)];
+}
