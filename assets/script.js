7f38b38c8336c01cdd42b88138b10f20d94ad357
muturich <calvinrich20@gmail.com>
2022-11-24 10:51:25 -0600

    add some style

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,17 @@
 function Getinfo(){
-const newName = document.getElementById ("cityInput");
-const cityName = document.getElementById ("cityName");
-cityName.innerHTML ="--"+newName.value+"--"
-}
-fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=ed61eb31d1ec0933f7ad88885d73f8bc")
+
+var newName = document.getElementById ("cityInput");
+var cityName = document.getElementById ("cityName");
+cityName.innerHTML ="--"+newName.value+"--";
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=ed61eb31d1ec0933f7ad88885d73f8bc')
 .then (response => response.json ())
 .then (data =>{
+
     for (i=0;i<5;i++){
-        document.getElementById("day" + (i+1)+ "Min").innerHTML = "Min:" + Number(data.list[i].main.temp_min -278.42).toFixed(1)+"°";
+        document.getElementById("day" + (i+1)+ "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min -278.42).toFixed(1)+"°";
     }
+
     for (i=0;i<5;i++){
         document.getElementById("day" + (i+1)+ "Max").innerHTML = "Max:" + Number(data.list[i].main.temp_max -279.26).toFixed(1)+"°";
     }
@@ -16,26 +19,29 @@ fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appi
     for (i=0;i<5;i++){
         document.getElementById("img" +(i+1)).src=" http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon+".png";
     }
+
+    console.log(data)
+
 })
 .catch(err => alert("Something Went Wrong"))
 
-
+}
 function DefaultScreen(){
     document.getElementById("cityInput").defaultValue ="Kigali";
     Getinfo();
 }
-const d=new Date();
-const weekday=["Sundays","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var d =new Date();
+var weekday=["Sundays","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",];
 
 function CheckDay(day){
     if (day +d.getDay()>6){
-        return day +d.getDay() -7;
+        return day + d.getDay() -7;
     }
     else{
-        return day +d.getDay();
+        return day + d.getDay();
     }
 }
 
 for (i=0;i<5;i++){
-    document.getElementById("day"+(i+1)).innerHTML=weekday[CheckDay(i)];
+    document.getElementById("day" + (i+1)).innerHTML= weekday[CheckDay(i)];
 }
